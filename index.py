c1596537d9aaf7aa95d41944fc4dde0176b396ff
tolumide-ng <tolumideshopein@gmail.com>
2021-01-11 00:35:44 +0100

    URL shortener

---
+++ index.py
@@ -0,0 +1,10 @@
+import requests
+api_key = "bfdf7469835fe0607f6d26faec4aeae2a96c8"
+url = input()
+api_url = f"https://cutt.ly/api/api.php?key={api_key}&short={url}"
+data = requests.get(api_url).json()["url"]
+if data["status"] == 7:
+    shortened_url = data["shortLink"]
+    print("Shortened URL: ", shortened_url)
+else:
+    print("[!] Error Shortening URL: ", data)
